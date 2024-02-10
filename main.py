dd49688fb1a971a03a768e7bad9cae33be2c33a9
PaloDeEscoba <147647595+jesusmremon@users.noreply.github.com>
2024-02-10 17:30:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,11 +26,15 @@ if flow:
   completion = client.chat.completions.create(
         model="gpt-4-1106-preview",
         messages=[
-          {"role": "system", "content": "You are a worldwide renounce marketer, you are able to manage the best marketing campaigns and to understand client needs, when a user gives you a company description and data to interpret."},
-          {"role": "user", "content": f"B"}
+          {"role": "system", "content": "You are a worldwide renounce marketer, you can manage the best marketing campaigns and understand client needs when a user gives you a company description and data to interpret."},
+          {"role": "user", "content": f"Write a marketing report with the next structure [Relation between the Company product and the new category found][A marketing strategy for LinkedIn, Instagram, Twitter and TikTok, based in the insights gained][Microinfluencers er social media network that can help the company]"}
         ]
       )
 
   output = completion.choices[0].message.content
+
+  st.info(output)
+
+  
 
   st.info(output)
